84199d516ff518e4a91deb364f9ff16676dd7a01
Sohaib <mmubashir.bscs24seecs@seecs.edu.pk>
2026-04-05 01:17:55 +0500

    oops forgot a file

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,5 +1,20 @@
 import api from './index'
 
+export const getPosts = async () => {
+  const response = await api.get('/posts')
+  return response.data
+}
+
+export const getPostById = async (postId) => {
+  const response = await api.get(`/posts/${postId}`)
+  return response.data
+}
+
+export const createPost = async (caption) => {
+  const response = await api.post('/posts', { caption })
+  return response.data
+}
+
 export const likePost = async (postId) => {
   const response = await api.put(`/posts/${postId}/like`)
   return response.data
